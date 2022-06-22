df8169b4f272918bf2033046c23b9bdb28aba3f6
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-06-22 17:08:25 -0400

    basic review of functions

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -1,13 +1,44 @@
 //Create a conditonal that checks their age
-//If under 16, tell them they can not drive
-//If under 18, tell them they can't hate from outside the club, because they can't even get in
-//If under 21, tell them they can not drink
-//If under 25, tell them they can not rent cars affordably
-//If under 30, tell them they can not rent fancy cars affordably
-//If under over 30, tell them there is nothing left to look forward too
 
 
 //--- Harder
 //On click of the h1
 //Take the value from the input
 //Place the result of the conditional in the paragraph
+
+// change the age variable to equal document.querySelector("input").value to target the dynamic value a user inputs
+
+document.querySelector('h1').addEventListener('click', ageCheck);
+
+let p = document.querySelector('p');
+
+function ageCheck() {
+  let age = Number(document.querySelector("input").value);
+  //If under 16, tell them they can not drive
+  if (age < 16) {
+    p.innerText = "you can't drive";
+  }
+  //If under 18, tell them they can't hate from outside the club, because they can't even get in
+  else if (age < 18) {
+    p.innerText = "can't hate from outside the club, because they can't even get in";
+  }
+  //If under 21, tell them they can not drink
+  else if (age < 21) {
+    p.innerText = "cannot drink";
+  }
+  //If under 25, tell them they can not rent cars affordably
+  else if (age < 25) {
+    p.innerText = "cannot rent cars affordably";
+  }
+  //If under 30, tell them they can not rent fancy cars affordably
+  else if (age < 30) {
+    p.innerText = "cannot rent fancy cars affordably";
+  }
+  //If under over 30, tell them there is nothing left to look forward too
+  else if (age > 30) {
+    p.innerText = "nothing left";
+  }
+  else {
+    p.innerText = "loveya";
+  }
+}
--- super-review/simple-functions/js/main.js
+++ super-review/simple-functions/js/main.js
@@ -1,12 +1,44 @@
 //---Easy
 //create a function that subtracts two numbers and alerts the difference
-
+function sub(one, two) {
+  console.log(one - two);
+}
+sub(3, 2);
 //create a function that divides three numbers and console logs the quotient
-
+function divs(num1, num2, num3) {
+  console.log(num1 / num2 / num3);
+}
+divs(10, 2, 1);
 //create a function that multiplys three numbers and returns the product
 
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
+function med(n1, n2, n3) {
+  let add = n1 + n2;
+  let remainder = add % n3;
+  console.log(remainder);
+  // return (n1 + n2) % n3;
+}
+
+med(10, 10, 2);
+// let remainder = med(10, 10, 2);
+// console.log(remainder);
 
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
+function hard(n1, n2, n3, n4) {
+  let multi = n1 * n2;
+  if (multi > 100) {
+    let add = n3 + n4;
+    console.log(add)
+  } else if (multi < 100) {
+    let sub = n3 - n4;
+    console.log(sub)
+  } else {
+    let multhree = n1 * n2 * n3;
+    let remainder = multhree / n4;
+    console.log(remainder);
+  }
+}
+
+hard(10, 9, 11, 10)

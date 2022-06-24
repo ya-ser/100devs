f4b53f830b06d52c50d2eef2d222ce0926d64146
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2022-06-24 12:32:18 -0400

    complete conditional for weekends included, have changes rendered in dom

--- class12-materials/class-weekend-boring/js/main.js
+++ class12-materials/class-weekend-boring/js/main.js
@@ -4,17 +4,21 @@ function check() {
 
   const day = document.querySelector('#day').value
 
+  const h2 = document.querySelector('#placeToSee')
+
   //Conditionals go here
-  if (day === "Monday" || "monday") {
-    console.log("day is monday")
-  } else if (day === "Tuesday" || "tuesday") {
-    console.log("day is tuesday")
-  } else if (day === "Wednesday" || "wednesday") {
-    console.log("day is wednesday")
-  } else if (day === "Thursday" || "thursday") {
-    console.log("day is thursday")
-  } else if (day === "Friday" || "friday") {
-    console.log("day is friday")
+  if (day === "Monday" || day === "monday") {
+    h2.innerText = "day is monday"
+  } else if (day === "Tuesday" || day === "tuesday") {
+    h2.innerText = "day is tuesday"
+  } else if (day === "Wednesday" || day === "wednesday") {
+    h2.innerText = "day is wednesday"
+  } else if (day === "Thursday" || day === "thursday") {
+    h2.innerText = "day is thursday"
+  } else if (day === "Friday" || day === "friday") {
+    h2.innerText = "day is friday"
+  } else if (day === "Saturday" || day === "saturday" || "Sunday" || day === "sunday") {
+    h2.innerText = "its the weekend"
   }
 
 }
